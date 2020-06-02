c279f11c402b8dc3adc6283219062b0692aac3fc
Xela20S <alex.abe.short@gmail.com>
2020-06-01 23:11:36 -0700

    Added paddle motion + unfinished ball script

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D Rb2D = gameObject.AddComponent<Rigidbody2D>();
+        Rb2D.gravityScale = 0;
+        Rb2D.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        this.transform.position = new Vector3(0, 0, 0);
+        Rb2D.velocity = new Vector2(0, -3);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = new Vector3(0, -3.5f, 0);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 myPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = new Vector3(myPos.x, -3.5f, 0);
+
+    }
+}
